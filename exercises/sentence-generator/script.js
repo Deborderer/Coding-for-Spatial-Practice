ce9c71eded110da7d058b0db85a838371cdd9018
Minsung Kim <mk4513@columbia.edu>
2021-11-09 20:53:03 -0500

    11092021

---
+++ exercises/sentence-generator/script.js
@@ -0,0 +1,15 @@
+ console.log('it works')
+
+ let nouns = ["heart", "rainbow", "ocean"];
+ let verbs = ["look", "make", "continue"];
+ let adjectives = ["good", "different", "possible"];
+
+
+let noun = nouns[Math.floor(Math.random() * nouns.length)];
+let verb = verbs[Math.floor(Math.random() * verbs.length)];
+let adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+
+
+let sentence = `My ${noun} leaps ${adjectives} when I ${verb} a rainbow in the sky.`;
+
+console.log(sentence);
